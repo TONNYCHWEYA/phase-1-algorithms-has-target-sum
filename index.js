66e339cb5269a77eb8f32f50cca26b084acf32b0
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-18 13:50:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for(let i = 0; i < array.length; i++){
+    for(let j = i + 1; j < array.length; j++){
+      let sum = array[i] + array[j];
+      if(sum === target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n**2) Quadratic time complexity
+  0(n)
 */
 
 /* 
   Add your pseudocode here
+
+  Iterete over the array
+  Adding an element of the array to every other elemt in the array 
+  Compare the result of the added array to the target array and return true 
+  else return false
 */
 
 /*
@@ -29,6 +46,22 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4))
 }
 
 module.exports = hasTargetSum;
